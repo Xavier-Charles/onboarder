ab983afa3304763b6b793a99309e9dc3611c3291
Xharles <charlesnzubechukwu000@gmail.com>
2023-05-07 19:36:51 +0100

    reconfigure router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,15 +1,30 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { cloneElement } from "react";
+import { useLocation, useRoutes } from "react-router-dom";
 import Home from "./pages/Home";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
+import { AnimatePresence } from "framer-motion";
+import Quest from "./pages/Quest";
 
 function Router() {
-  return <RouterProvider router={router} />;
+  const element = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/q/bnb-greenfield",
+      element: <Quest />,
+    },
+  ]);
+
+  const location = useLocation();
+
+  if (!element) return null;
+
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      {cloneElement(element, { key: location.pathname })}
+    </AnimatePresence>
+  );
 }
 
 export default Router;
