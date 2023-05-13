8993a9dd0b0cad1841dd38614cb81d62ef739ba4
Xharles <charlesnzubechukwu000@gmail.com>
2023-05-13 17:40:08 +0100

    reduce zIndex of Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className="py-7 absolute w-full">
       <Container>
-        <nav className="relative z-50 flex justify-between">
+        <nav className="relative z-10 flex justify-between">
           <div className="flex items-center md:gap-x-12">
             <Link to="/" aria-label="Home">
               <Logo className="h-32 w-auto" />
