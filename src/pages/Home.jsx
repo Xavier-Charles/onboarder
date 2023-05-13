37756349b199d0c0ef14416986cf3c3c13b8d79c
Xharles <charlesnzubechukwu000@gmail.com>
2023-05-13 11:32:02 +0100

    add asroll to top

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
+import { useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { Header } from "../components/Header";
 import { Hero } from "../components/Hero";
 import Quests from "../components/Quests";
 import { Learn } from "../components/Learn";
 import { Footer } from "../components/Footer";
+import { scrollToTop } from "../utils/scrollToTop";
 
 export default function Home() {
+  useEffect(() => {
+    scrollToTop();
+  }, []);
 
   return (
     <article>
--- src/pages/Quest.jsx
+++ src/pages/Quest.jsx
@@ -1,55 +1,30 @@
-import { Link } from "@reach/router";
-import { motion, useIsPresent } from "framer-motion";
+import { useEffect } from "react";
+import TransitionAnimation from "../components/quest/transitionAnimation";
+import Hero from "../components/quest/hero";
+import { Helmet } from "react-helmet";
+import { Header } from "../components/Header";
+import { scrollToTop } from "../utils/scrollToTop";
 
 const Quest = () => {
-  const isPresent = useIsPresent();
+  useEffect(() => {
+    scrollToTop();
+  }, []);
 
   return (
-    <article>
-      <div key="quest" className="bg-oxford">
-        <Link to="/" className="text-magwhite underline text-3xl">
-          Home
-        </Link>
-        <h1>Quest</h1>
-        <p className="py-40">Quest</p>
-      </div>
-      <motion.div
-        initial={{ scaleX: 1 }}
-        animate={{
-          scaleX: 0,
-          transition: { duration: 0.5, ease: "circOut", delay: 0.5 },
-        }}
-        style={{ originX: isPresent ? 0 : 1 }}
-        className="bg-sunglow/95 fixed top-0 left-0 right-0 bottom-0 z-10"
-      />
-      <motion.div
-        initial={{ scaleX: 1 }}
-        animate={{
-          scaleX: 0,
-          transition: { duration: 0.5, ease: "circOut", delay: 0.4 },
-        }}
-        style={{ originX: isPresent ? 0 : 1 }}
-        className="bg-teal/95 fixed top-0 left-0 right-0 bottom-0 z-20"
-      />
-      <motion.div
-        initial={{ scaleX: 1 }}
-        animate={{
-          scaleX: 0,
-          transition: { duration: 0.5, ease: "circOut", delay: 0.2 },
-        }}
-        style={{ originX: isPresent ? 0 : 1 }}
-        className="bg-pink/95 fixed top-0 left-0 right-0 bottom-0 z-30"
-      />
-      <motion.div
-        initial={{ scaleX: 1 }}
-        animate={{
-          scaleX: 0,
-          transition: { duration: 0.5, ease: "circOut", delay: 0.1 },
-        }}
-        style={{ originX: isPresent ? 0 : 1 }}
-        className="bg-oxford/95 fixed top-0 left-0 right-0 bottom-0 z-40"
-      />
-    </article>
+    <section>
+      <Helmet>
+        <title>
+          Onboarder - Seamlessly Onboard to Web3 Products and Protocols
+        </title>
+        <meta
+          name="description"
+          content="Onboarder is a Web3 onboarding solution that allows users to seamlessly onboard to decentralized products and protocols."
+        />
+      </Helmet>
+      <Header />
+      <Hero />
+      <TransitionAnimation />
+    </section>
   );
 };
 
