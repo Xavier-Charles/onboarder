1d903cac419b787f934b813c2732ee3beda345b5
Xharles <charlesnzubechukwu000@gmail.com>
2023-05-13 10:48:52 +0100

    add high z index to transition animation

--- src/components/quest/TransitionAnimation.jsx
+++ src/components/quest/TransitionAnimation.jsx
@@ -4,7 +4,7 @@ const TransitionAnimation = () => {
   const isPresent = useIsPresent();
 
   return (
-    <>
+    <div className="z-[9999] relative">
       <motion.div
         initial={{ scaleX: 1 }}
         animate={{
@@ -12,7 +12,7 @@ const TransitionAnimation = () => {
           transition: { duration: 0.5, ease: "circOut", delay: 0.5 },
         }}
         style={{ originX: isPresent ? 0 : 1 }}
-        className="bg-sunglow/95 fixed top-0 left-0 right-0 bottom-0 z-10"
+        className="bg-sunglow fixed top-0 left-0 right-0 bottom-0 z-10"
       />
       <motion.div
         initial={{ scaleX: 1 }}
@@ -41,7 +41,7 @@ const TransitionAnimation = () => {
         style={{ originX: isPresent ? 0 : 1 }}
         className="bg-oxford/95 fixed top-0 left-0 right-0 bottom-0 z-40"
       />
-    </>
+    </div>
   );
 };
 
