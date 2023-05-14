666d3aa23d7e4514d92d90d3b25c352c31bc0396
Xharles <charlesnzubechukwu000@gmail.com>
2023-05-14 19:10:21 +0100

    seperate q7a from verify

--- src/components/lesson/Lesson2.jsx
+++ src/components/lesson/Lesson2.jsx
@@ -7,6 +7,8 @@ import clsx from "clsx";
 import useClickOutside from "../../hooks/useClickOutside";
 import CardNav from "./CardNav";
 import LessonComplete from "./LessonComplete";
+import QandA from "./QandA";
+import Verify from "./Verify";
 
 const BlockQuoteSVG = ({ classes }) => (
   <svg
@@ -124,53 +126,20 @@ const Lesson2 = ({ onLessonComplete }) => {
                       >
                         {showCardQA ? (
                           <>
-                            {Object.values(selectedItem.options).map(
-                              (option) => {
-                                const optionSelected =
-                                  selectedOptionId &&
-                                  selectedOptionId === option.id;
-                                const isCorrect =
-                                  selectedItem.answer === option.id;
-                                return (
-                                  <motion.button
-                                    key={option.id}
-                                    whileHover={{ scale: 1.05 }}
-                                    whileTap={{ scale: 0.95 }}
-                                    onClick={() => {
-                                      setSelectedOptionId(option.id);
-                                      if (isCorrect) {
-                                        setCardProgress((prev) => {
-                                          return prev.map((item) => {
-                                            if (item.id === selectedItem.id) {
-                                              return {
-                                                ...item,
-                                                completed: true,
-                                              };
-                                            }
-                                            return item;
-                                          });
-                                        });
-                                      }
-                                    }}
-                                    className={clsx(
-                                      "col-span-1 divide-y my-2 divide-gray-200 rounded-lg bg-magwhite shadow w-[90%]",
-                                      optionSelected &&
-                                        (isCorrect ? "bg-teal" : "bg-red-500")
-                                    )}
-                                  >
-                                    <div className="flex w-full items-center justify-between space-x-6 p-6">
-                                      <h3
-                                        className={clsx(
-                                          "truncate text-sm font-medium",
-                                          optionSelected && "text-magwhite"
-                                        )}
-                                      >
-                                        {option.value}
-                                      </h3>
-                                    </div>
-                                  </motion.button>
-                                );
-                              }
+                            {selectedItem.type === "verify" ? (
+                              <QandA
+                                selectedItem={selectedItem}
+                                selectedOptionId={selectedOptionId}
+                                setSelectedOptionId={setSelectedOptionId}
+                                setCardProgress={setCardProgress}
+                              />
+                            ) : (
+                              <Verify
+                                selectedItem={selectedItem}
+                                selectedOptionId={selectedOptionId}
+                                setSelectedOptionId={setSelectedOptionId}
+                                setCardProgress={setCardProgress}
+                              />
                             )}
                           </>
                         ) : (
