d1f1b02a1ac4ed4b66eef365705e1bb24c269e7c
Xharles <charlesnzubechukwu000@gmail.com>
2023-05-21 15:12:42 +0100

    move questsList to api folder

---
+++ src/api/mockData/allQuests.js
@@ -0,0 +1,36 @@
+import bnbgreenfield from "../../assets/img/projects/bnbgreenfield.jpg";
+import nodereal from "../../assets/img/projects/nodereal.jpg";
+import axelar from "../../assets/img/projects/axelar.jpg";
+
+export const questsList = [
+  {
+    id: 1,
+    slug: "bnb-greenfield",
+    title: "BnB Greenfield",
+    start: "2023-05-10T09:30:00",
+    end: "2023-11-10T11:59:00",
+    img: bnbgreenfield,
+    imgPos: "object-right",
+    colors: ["#787522"],
+  },
+  {
+    id: 2,
+    slug: "nodereal",
+    title: "NodeReal",
+    start: "2023-05-25T09:30:00",
+    end: "2023-10-05T11:59:00",
+    img: nodereal,
+    imgPos: "object-right",
+    colors: ["#4d32c5"],
+  },
+  {
+    id: 3,
+    slug: "axelar",
+    title: "Axelar",
+    start: "2023-05-22T09:30:00",
+    end: "2023-07-31T11:59:00",
+    img: axelar,
+    imgPos: "object-left",
+    colors: ["#1ea4c2"],
+  },
+];
