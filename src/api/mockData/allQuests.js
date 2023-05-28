61ff70204a3ab15465ecb0b6a92580d9abd8f2f9
Xharles <charlesnzubechukwu000@gmail.com>
2023-05-28 15:10:06 +0100

    add azuro quest

--- src/api/mockData/allQuests.js
+++ src/api/mockData/allQuests.js
@@ -3,6 +3,7 @@ import nodereal from "../../assets/img/projects/nodereal.jpg";
 import axelar from "../../assets/img/projects/axelar.jpg";
 import theta from "../../assets/img/projects/theta.jpg";
 import gnosis from "../../assets/img/projects/gnosis.jpg";
+import azuro from "../../assets/img/projects/azuro.jpg";
 
 export const questsList = [
   {
@@ -14,26 +15,6 @@ export const questsList = [
     img: bnbgreenfield,
     imgPos: "object-right",
     colors: ["#787522"],
-  },
-  {
-    id: 2,
-    slug: "nodereal",
-    title: "NodeReal",
-    start: "2023-05-25T09:30:00",
-    end: "2023-10-05T11:59:00",
-    img: nodereal,
-    imgPos: "object-right",
-    colors: ["#4d32c5"],
-  },
-  {
-    id: 3,
-    slug: "axelar",
-    title: "Axelar",
-    start: "2023-05-22T09:30:00",
-    end: "2023-07-31T11:59:00",
-    img: axelar,
-    imgPos: "object-left",
-    colors: ["#1ea4c2"],
   },
   {
     id: 4,
@@ -54,5 +35,38 @@ export const questsList = [
     img: gnosis,
     imgPos: "object-center",
     colors: ["#3e6957"],
+  },
+  {
+    id: 2,
+    slug: "nodereal",
+    title: "NodeReal",
+    start: "2023-08-25T09:30:00",
+    end: "2023-10-05T11:59:00",
+    img: nodereal,
+    imgPos: "object-right",
+    commingSoon: true,
+    colors: ["#4d32c5"],
+  },
+  {
+    id: 3,
+    slug: "axelar",
+    title: "Axelar",
+    start: "2023-08-22T09:30:00",
+    end: "2023-11-30T11:59:00",
+    img: axelar,
+    imgPos: "object-left",
+    commingSoon: true,
+    colors: ["#1ea4c2"],
+  },
+  {
+    id: 6,
+    slug: "azuro",
+    title: "Azuro",
+    start: "2023-08-22T09:30:00",
+    end: "2023-10-31T11:59:00",
+    img: azuro,
+    imgPos: "object-left",
+    commingSoon: true,
+    colors: ["#1ea4c2"],
   },
 ];
