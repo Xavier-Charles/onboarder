b604cfd3d7f53e0baa11b491e92d2fa279fa0e0a
Xharles <charlesnzubechukwu000@gmail.com>
2023-06-01 09:40:25 +0100

    Create mantle hero

--- src/partners/mantle/MantleHero.jsx
+++ src/partners/mantle/MantleHero.jsx
@@ -1,25 +1,25 @@
 import { motion } from "framer-motion";
-import world from "../../assets/img/quests/world-gnosis.png";
+import mantle from "../../assets/img/quests/herobg.jpg";
 import { navigate } from "@reach/router";
 
 const MantleHero = ({ slug }) => {
   return (
-    <div className="bg-[#o] min-h-screen">
+    <div className="bg-[#000] min-h-screen">
+      <img
+        src={mantle}
+        alt="backgound-image"
+        className="absolute object-cover right-0 h-screen"
+      />
       <div
         alt="backgound-image"
-        className="absolute inset-0 bg-[linear-gradient(145deg,#3e6957,#133629)]"
+        className="absolute -top-20 -left-10 w-[300px] h-[300px] bg-[#65b3ae]"
+        style={{ filter: "blur(200px)" }}
       />
-      <img
-        src={world}
-        alt="backgound-image"
-        className="absolute object-contain bottom-0 right-0 w-[50%] h-[90%] "
-      />
-
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-32 sm:py-48 lg:py-56">
           <div className="max-w-2xl">
             <h1 className="mx-auto mt-10 max-w-2xl text-5xl font-bold text-left tracking-normal text-magwhite sm:text-7xl">
-              GNOSIS CHAIN
+              MANTLE NETWORK
             </h1>
             <p className="mt-6 text-lg leading-8 text-slate/90 text-left pr-16">
               Learn to build on infrastructure for the open-internet. A
@@ -31,13 +31,13 @@ const MantleHero = ({ slug }) => {
                 onClick={() => navigate(`/q/${slug}/lessons`)}
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
-                className="rounded-md bg-[#3e6957]/80 px-5 py-3.5 font-semibold text-magwhite shadow-sm hover:bg-[#3e6957] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sunglow"
+                className="rounded-md bg-[#294a48]/80 px-5 py-3.5 font-semibold text-magwhite shadow-sm hover:bg-[#294a48] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sunglow"
               >
                 Start Quest
               </motion.button>
               <a
                 target="_blank"
-                href="https://www.thetatoken.org/"
+                href="https://www.mantle.xyz/"
                 className="font-semibold leading-6 text-slate group relative w-[102px]"
                 rel="noreferrer"
               >
